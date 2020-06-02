900a883745db6731964c5503c0b42a6d7a5bfde1
Your Name <your.email@example.com>
2020-06-03 00:37:49 +0900

    update userscontroller and usersview

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user  = User.find(1)
+    @user2 = User.find(2)
   end
 
   def new
+    @user = User.new()  #新しいモデルオブジェクトを作成
   end
 
   def edit
+    @user  = User.find(1)
+    @user2 = User.find(2)
   end
 
   def create
